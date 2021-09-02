1e579f02c8b08a9678d9cddead607f1c2e98d525
nouhoumovic4 <namaiga@ktminnov.com>
2021-09-02 12:21:40 +0000

    SQFLITE FULL

--- lib/main.dart
+++ lib/main.dart
@@ -1,131 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/recipeListScreen.dart';
+import 'package:flutter_app/recipe_screen.dart';
 import 'package:transparent_image/transparent_image.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Recettes',
+      initialRoute: '/',
+      onGenerateRoute: (settings) => RouteGenerator.generateRoute(settings),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text("Mes Recettes"),
-        ),
-        body: ListView(
-          children: [
-            FadeInImage.memoryNetwork(
-              placeholder: kTransparentImage,
-              //    image: 'images/pizza.png',
-              image:
-                  'https://tounkalafia.com/wp-content/uploads/2019/11/GOURMANDE-02-65.jpg',
-              width: 600,
-              height: 240,
-              fit: BoxFit.cover,
-            ),
-            titlesection(),
-            buttonSection(),
-            descriptionSection()
-          ],
-        ),
 
-        // This trailing comma makes auto-formatting nicer for build methods.
-      ),
+      /* ReccipeScreen(
+        recipe: Recipe(
+            "Pizza Facile",
+            "Par Illyasse Guindo",
+            "https://tounkalafia.com/wp-content/uploads/2019/11/GOURMANDE-02-65.jpg",
+            "Dans un bol, faites un puis avec la farine, et mettez le sel d'un côté, la levure de l'autre (faites en sorte de ne pas mettre en contact le sel et la levure au démarrage) Ajoutez l'eau au centre, mélangez pour obtenir une pâte homogène, et pétrissez 10 minutes à la main sur le plan de travail ou à l'aide d'une machine à pain ou d'un robot avec le crochet pétrisseur \n Ajoutez l'huile d'olive et pétrissez encore 5 minutes \n Couvrez d'un film alimentaire au contact de la pâte, et laissez lever 1 heure à température ambiante, la pâte doit doubler de volume \n Chassez l'air de la pâte, en appuyant dessus avec la main, puis divisez la pâte en 2 ou 3 boules selon le nombre de pizzas souhaitées \n Laissez les boules de pâte reposer 30 minutes en les couvrant de film alimentairen",
+            false,
+            50),
+      ), */
     );
   }
+}
 
-  Container descriptionSection() {
-    return Container(
-      padding: const EdgeInsets.all(32),
-      child: Text(
-        "Dans un bol, faites un puis avec la farine, et mettez le sel d'un côté, la levure de l'autre (faites en sorte de ne pas mettre en contact le sel et la levure au démarrage) Ajoutez l'eau au centre, mélangez pour obtenir une pâte homogène, et pétrissez 10 minutes à la main sur le plan de travail ou à l'aide d'une machine à pain ou d'un robot avec le crochet pétrisseur \n Ajoutez l'huile d'olive et pétrissez encore 5 minutes \n Couvrez d'un film alimentaire au contact de la pâte, et laissez lever 1 heure à température ambiante, la pâte doit doubler de volume \n Chassez l'air de la pâte, en appuyant dessus avec la main, puis divisez la pâte en 2 ou 3 boules selon le nombre de pizzas souhaitées \n Laissez les boules de pâte reposer 30 minutes en les couvrant de film alimentairen",
-        softWrap: true,
-      ),
-    );
-  }
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (context) => RecipeListScreen());
+      case '/recipe':
+        return PageRouteBuilder(
+            pageBuilder: (context, animation, secondaryAnimation) =>
+                ReccipeScreen(recipe: settings.arguments),
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+              /*  var begin = Offset(0.0, 1.0);
+                var end = Offset.zero;
+                var curve = Curves.ease;
+                var tween = Tween(begin: begin, end: end)
+                    .chain(CurveTween(curve: curve));
+                return SlideTransition(
+                  position: animation.drive(tween),
+                  child: child,
+                ); */
+              animation =
+                  CurvedAnimation(parent: animation, curve: Curves.ease);
+              return FadeTransition(
+                opacity: animation,
+                child: child,
+              );
+            });
 
-  Container buttonSection() {
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            _buildButtonColumn(Colors.indigo, Icons.comment, "COMMENT "),
-            _buildButtonColumn(Colors.indigo, Icons.share, "SHARE ")
-          ],
-        ),
-      ),
-    );
-  }
-
-  Container titlesection() {
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children: [
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.only(bottom: 8),
-                    child: Text(
-                      "Pizza Facile",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                    ),
+      default:
+        return MaterialPageRoute(
+            builder: (context) => Scaffold(
+                  appBar: AppBar(
+                    title: Text("Error"),
+                    centerTitle: true,
                   ),
-                  Text(
-                    "Par Guindo",
-                    style: TextStyle(color: Colors.grey[500], fontSize: 16),
+                  body: Center(
+                    child: Text("Page not Found"),
                   ),
-                ],
-              ),
-            ),
-            Icon(
-              Icons.favorite,
-              color: Colors.red,
-            ),
-            Text("55")
-          ],
-        ),
-      ),
-    );
-  }
-
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      children: [
-        Container(
-          padding: const EdgeInsets.only(bottom: 8),
-          child: Icon(icon, color: color),
-        ),
-        Text(
-          label,
-          style: TextStyle(
-              fontSize: 14, fontWeight: FontWeight.w400, color: color),
-        )
-      ],
-    );
+                ));
+    }
   }
 }
